ddba05092945127abe904436b2bd9baca366615b
pkowalecki <kowalecki13pl@o2.pl>
2020-04-09 17:45:44 +0200

    Model

---
+++ src/main/java/pl/kowalecki/carsapi/repository/CarRepository.java
@@ -0,0 +1,14 @@
+package pl.kowalecki.carsapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pl.kowalecki.carsapi.model.Car;
+
+import java.util.List;
+
+@Repository
+public interface CarRepository extends JpaRepository<Car, Long> {
+
+    List<Car> findAllById(Long id);
+
+}
---
+++ src/main/java/pl/kowalecki/carsapi/service/CarService.java
@@ -0,0 +1,13 @@
+package pl.kowalecki.carsapi.service;
+
+import pl.kowalecki.carsapi.model.Car;
+
+import java.util.List;
+
+
+public interface CarService {
+
+    List<Car> getAllCars();
+    boolean addCar(Car car);
+
+}
